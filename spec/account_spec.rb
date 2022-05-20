fbcaef77057a81b23cbc51552d9a17361211346e
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-20 16:01:23 +0100

    test: account balance increases by 1000 when deposit of 1000 made

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,12 @@ describe Account do
   it 'initializes with a balance set to 0' do
     expect(account.balance).to eq(0)
   end
+
+  describe '#deposit' do
+    describe 'adds to the balance' do
+      it 'increases the balance by 1000 when 1000 is added' do
+        expect { account.deposit(1000) }.to change { account.balance }.from(0).to(1000)
+      end
+    end
+  end
 end
