e87da13a31605e01126c7a7f3c3b399db7147ac4
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-20 18:52:46 +0100

    test: print statement prints multiple transactions each on a different line

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -97,15 +97,22 @@ describe Account do
       it "prints 'DD/MM/YYYY || 3000.00 || || 3000.00' as statement following single 3000 deposit" do
         account.deposit(3000)
         expect { account.print_statement }.to output(
-          %r{\d{2}/\d{2}/\d{4} || 3000.00 ||  || 3000.00}
+          /\d{2}\/\d{2}\/\d{4} \|\| 3000.00 \|\|  \|\| 3000.00/
         ).to_stdout
       end
 
       it "prints 'DD/MM/YYYY || 5000.00 || || 5000.00' as statement following single 3000 deposit" do
         account.deposit(5000)
         expect { account.print_statement }.to output(
-          %r{\d{2}/\d{2}/\d{4} || 5000.00 ||  || 5000.00}
+          /\d{2}\/\d{2}\/\d{4} \|\| 5000.00 \|\|  \|\| 5000.00/
         ).to_stdout
+      end
+
+      it "prints each transaction on a separate line" do
+        account.deposit(5000)
+        account.deposit(2000)
+        expect { account.print_statement }.to output(
+          /\d{2}\/\d{2}\/\d{4} \|\| 5000.00 \|\|  \|\| 5000.00\n\d{2}\/\d{2}\/\d{4} \|\| 2000.00 \|\|  \|\| 3000.00/).to_stdout
       end
     end
   end
