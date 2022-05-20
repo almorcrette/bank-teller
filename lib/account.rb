3abd12da51b708ed40aeb1519c6f10faefae7385
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-20 18:59:40 +0100

    feat: print statement prints multiple transactions, same line

--- lib/account.rb
+++ lib/account.rb
@@ -20,9 +20,12 @@ class Account
   end
 
   def print_statement
-    credit_two_decimals = sprintf("%.2f", transactions[0][:credit])
-    balance_two_decimals = sprintf("%.2f", transactions[0][:balance])
-    print "#{@transactions[0][:date]} || #{credit_two_decimals} || #{transactions[0][:debit]} || #{balance_two_decimals}"
+    @transactions.each do |transaction|
+      credit_two_decimals = sprintf("%.2f", transaction[:credit])
+      balance_two_decimals = sprintf("%.2f", transaction[:balance])
+      print "#{transaction[:date]} || #{credit_two_decimals} || #{transaction[:debit]} || #{balance_two_decimals}"
+    end
+    
   end
 
   private
