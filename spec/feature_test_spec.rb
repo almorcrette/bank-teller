d9024858974a0af343711575ceb93e949cbf1a96
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-23 19:25:01 +0100

    test: work on feature test

--- spec/feature_test_spec.rb
+++ spec/feature_test_spec.rb
@@ -8,10 +8,6 @@ describe 'Bank Teller program' do
     acc.deposit(1000)
     acc.deposit(2000)
     acc.withdraw (500)
-    expect { acc.print_statement }.to output(
-"date || credit || debit || balance
-\d{2}\/\d{2}\/\d{4} \|\|  \|\| 500.00 \|\| 2500.00
-\d{2}\/\d{2}\/\d{4} \|\| 2000.00 \|\|  \|\| 3000.00
-\d{2}\/\d{2}\/\d{4} \|\| 1000.00 \|\|  \|\| 1000.00\n").to_stdout
+    expect { acc.print_statement }.to output(/date || credit || debit || balance\n\d{2}\/\d{2}\/\d{4} \|\|  \|\| 500.00 \|\| 2500.00\n\d{2}\/\d{2}\/\d{4} \|\| 2000.00 \|\|  \|\| 3000.00\n\d{2}\/\d{2}\/\d{4} \|\| 1000.00 \|\|  \|\| 1000.00\n/).to_stdout
   end
 end
