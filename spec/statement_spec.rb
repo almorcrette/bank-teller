bfc497e1a1b99119908a6de56fcc4fa6b81bf056
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-23 15:05:57 +0100

    correction: test: Statement#log_transaction adds transaction to log:

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -3,11 +3,11 @@ require './lib/statement'
 describe Statement do
   let(:statement) { described_class.new }
   let(:transaction) { double :transaction }
-  
+
   describe '#log_transaction' do
     it 'adds the transaction to the transactions log' do
       statement.log_transaction(transaction)
-      expect { statement.transaction }.to include(transaction)
+      expect { statement.transactions }.to include(transaction)
     end
   end
 end
