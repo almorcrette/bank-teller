bb94536da2aac59b85b01c5de1f17ab95f252492
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-23 14:49:22 +0100

    feat: Transaction.create passes type: credit and amount to initialize with amount in credit field

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,14 +1,15 @@
 class Transaction
 
-  def self.create(balance:)
-    Transaction.new(balance)
+  def self.create(type:, amount:, balance:)
+    Transaction.new(type, amount, balance)
   end
 
-  attr_reader :date, :balance
+  attr_reader :date, :balance, :credit
 
-  def initialize(balance)
+  def initialize(type, amount, balance)
     @date = Time.now.strftime('%d/%m/%Y')
     @balance = balance
+    @credit = amount
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,27 +5,37 @@ describe Transaction do
   describe '::create' do
     describe 'initiates a new transaction instance' do
       it 'with a datestamp' do
-        transaction = Transaction.create(balance: 1000)
+        transaction = Transaction.create(type: :credit, amount: 1000, balance: 1000)
         expect(transaction.date).to match(%r{\d{2}/\d{2}/\d{4}})
       end
 
       describe 'with balance data' do
         it 'records a balance of 1000 when passed 1000 as balance argument' do
-          transaction = Transaction.create(balance: 1000)
+          transaction = Transaction.create(type: :credit, amount: 1000, balance: 1000)
           expect(transaction.balance).to eq(1000)
         end
 
         it 'records a balance of 2000 when passed 2000 as balance argument' do
-          transaction = Transaction.create(balance: 2000)
+          transaction = Transaction.create(type: :credit, amount: 1000, balance: 2000)
           expect(transaction.balance).to eq(2000)
         end
 
         it 'records a balance of 2000 when passed 2000 as balance argument' do
-          transaction = Transaction.create(balance: 3000)
+          transaction = Transaction.create(type: :credit, amount: 1000, balance: 3000)
           expect(transaction.balance).to eq(3000)
         end
+      end
 
+      describe 'with an amount and a defined type (credit or debit' do
+        it 'passes 1000 as credit if arguments type: credit and amount: 1000' do
+          transaction = Transaction.create(type: :credit, amount: 1000, balance: 3000)
+          expect(transaction.credit).to eq(1000)
+        end
 
+        it 'passes 2000 as credit if arguments type: credit and amount: 2000' do
+          transaction = Transaction.create(type: :credit, amount: 2000, balance: 3000)
+          expect(transaction.credit).to eq(2000)
+        end
       end
     end
   end
