ce384a239f0b9f8ccb0780696c5464c8e9637412
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-23 18:07:24 +0100

    feat: dependency injection of statement and Transaction into account

--- lib/account.rb
+++ lib/account.rb
@@ -1,49 +1,63 @@
 # frozen_string_literal: true
 
+require_relative('./transaction')
+require_relative('./statement')
+
 # Account for holding customer balance and executing bank teller functions
 class Account
+
+  # Initiates with a statement, instance of Statement
+  # Initiates with a Transaction class
   attr_reader :balance, :transactions
 
-  def initialize
+  def initialize(transaction_class = Transaction, statement = Statement.new)
     @balance = 0.00
+    @statement = statement
+    @transaction_class = transaction_class
     @transactions = []
   end
 
   def deposit(amount)
     @balance += amount
-    log_transaction(:credit, amount)
+    @statement.log_transaction(@transaction_class.create(type: :credit, amount: amount, balance: @balance))
+    # log_transaction(:credit, amount)
   end
 
   def withdraw(amount)
     @balance -= amount
-    log_transaction(:debit, amount.to_f.round(2))
+    @statement.log_transaction(@transaction_class.create(type: :debit, amount: amount, balance: @balance))
+    # log_transaction(:debit, amount.to_f.round(2))
   end
 
   def print_statement
-    print "date || credit || debit || balance\n"
-    @transactions.reverse.each do |transaction|
-      credit_formatted = format_amount(transaction[:credit])
-      debit_formatted = format_amount(transaction[:debit])
-      balance_formatted = format_amount(transaction[:balance])
-      print "#{transaction[:date]} || #{credit_formatted} || #{debit_formatted} || #{balance_formatted}\n"
-    end
+    @statement.display
   end
+
+  # def print_statement
+  #   print "date || credit || debit || balance\n"
+  #   @transactions.reverse.each do |transaction|
+  #     credit_formatted = format_amount(transaction[:credit])
+  #     debit_formatted = format_amount(transaction[:debit])
+  #     balance_formatted = format_amount(transaction[:balance])
+  #     print "#{transaction[:date]} || #{credit_formatted} || #{debit_formatted} || #{balance_formatted}\n"
+  #   end
+  # end
 
   private
 
-  def log_transaction(transaction_type, amount)
-    today = Time.now.strftime('%d/%m/%Y')
+  # def log_transaction(transaction_type, amount)
+  #   today = Time.now.strftime('%d/%m/%Y')
 
-    @transactions.push(
-      {
-        date: today,
-        "#{transaction_type}": amount,
-        balance: @balance
-      }
-    )
-  end
+  #   @transactions.push(
+  #     {
+  #       date: today,
+  #       "#{transaction_type}": amount,
+  #       balance: @balance
+  #     }
+  #   )
+  # end
 
-  def format_amount(amount)
-    format('%.2f', amount) if amount
-  end
+  # def format_amount(amount)
+  #   format('%.2f', amount) if amount
+  # end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,7 +9,12 @@
 require './lib/account'
 
 describe Account do
-  let(:account) { described_class.new }
+  let(:transaction) { double :transaction }
+  let(:transaction_class) {
+    double :transaction_class,
+    create: transaction }
+  let(:statement) { double :statement }
+  let(:account) { described_class.new(transaction_class, statement) }
 
   it 'initializes with a balance set to 0' do
     expect(account.balance).to eq(0)
@@ -18,122 +23,142 @@ describe Account do
   describe '#deposit' do
     describe 'adds to the balance' do
       it 'increases the balance by 1000 when 1000 is deposited' do
+        allow(statement).to receive(:log_transaction)
         expect { account.deposit(1000) }.to change { account.balance }.from(0).to(1000)
       end
     end
 
-    describe 'adds a credit transaction to the list of transactions' do
-      it 'adds hash with credit key and 1000 value to transactions when 1000 is deposited' do
-        account.deposit(1000)
-        expect(account.transactions[0][:credit]).to eq(1000)
-      end
-
-      it 'adds hash with credit key and 2000 value to transactions when 2000 is deposited' do
-        account.deposit(2000)
-        expect(account.transactions[0][:credit]).to eq(2000)
-      end
-
-      describe 'transaction is dated' do
-        it 'adds hash with date key and a value of date formatted DD/MM/YYYY to transactions' do
-          account.deposit(1000)
-          expect(account.transactions[0][:date]).to match(%r{\d{2}/\d{2}/\d{4}})
-        end
-      end
-
-      describe 'transaction includes updated balance' do
-        it 'adds hash with balance key and updated 1000 balance as value to initial 1000 credit transaction' do
-          account.deposit(1000)
-          expect(account.transactions[0][:balance]).to eq(1000)
-        end
-
-        it 'adds hash with balance key and updated 2000 balance as value to initial 2000 credit transaction' do
-          account.deposit(2000)
-          expect(account.transactions[0][:balance]).to eq(2000)
-        end
-      end
+    it 'logs credit transaction in the statement' do
+      expect(transaction_class).to receive(:create)
+      expect(statement).to receive(:log_transaction).with(transaction)
+      account.deposit(1000)
     end
+
+
+
+    # describe 'adds a credit transaction to the list of transactions' do
+    #   it 'adds hash with credit key and 1000 value to transactions when 1000 is deposited' do
+    #     account.deposit(1000)
+    #     expect(account.transactions[0][:credit]).to eq(1000)
+    #   end
+
+    #   it 'adds hash with credit key and 2000 value to transactions when 2000 is deposited' do
+    #     account.deposit(2000)
+    #     expect(account.transactions[0][:credit]).to eq(2000)
+    #   end
+
+    #   describe 'transaction is dated' do
+    #     it 'adds hash with date key and a value of date formatted DD/MM/YYYY to transactions' do
+    #       account.deposit(1000)
+    #       expect(account.transactions[0][:date]).to match(%r{\d{2}/\d{2}/\d{4}})
+    #     end
+    #   end
+
+    #   describe 'transaction includes updated balance' do
+    #     it 'adds hash with balance key and updated 1000 balance as value to initial 1000 credit transaction' do
+    #       account.deposit(1000)
+    #       expect(account.transactions[0][:balance]).to eq(1000)
+    #     end
+
+    #     it 'adds hash with balance key and updated 2000 balance as value to initial 2000 credit transaction' do
+    #       account.deposit(2000)
+    #       expect(account.transactions[0][:balance]).to eq(2000)
+    #     end
+    #   end
+    # end
   end
 
   describe '#withdraw' do
     describe 'removes from the balance' do
       it 'reduces the balance by 1000 when 1000 is withdrawn' do
+        allow(statement).to receive(:log_transaction)
         account.deposit(1000)
         expect { account.withdraw(1000) }.to change { account.balance }.from(1000).to(0)
       end
     end
 
-    describe 'adds a debit transaction to the list of transactions' do
-      it 'adds hash with debit key and 1000 value to transactions when 1000 is withdrawn' do
-        account.deposit(2000)
-        account.withdraw(1000)
-        expect(account.transactions[-1][:debit]).to eq(1000)
-      end
-
-      it 'adds hash with debit key and 2000 value to transactions when 2000 is withdrawn' do
-        account.deposit(3000)
-        account.withdraw(2000)
-        expect(account.transactions[-1][:debit]).to eq(2000)
-      end
-
-      describe 'transaction is dated' do
-        it 'adds hash with date key and a value of date formatted DD/MM/YYYY to transactions' do
-          account.deposit(2000)
-          account.withdraw(1000)
-          expect(account.transactions[-1][:date]).to match(%r{\d{2}/\d{2}/\d{4}})
-        end
-      end
-
-      describe 'transaction includes updated balance' do
-        it 'adds hash with balance key and updated 1000 balance as value to 2000 debit transaction from 3000 balance' do
-          account.deposit(3000)
-          account.withdraw(2000)
-          expect(account.transactions[-1][:balance]).to eq(1000)
-        end
-        it 'adds hash with balance key and updated 2000 balance as value to 1000 debit transaction from 3000 balance' do
-          account.deposit(3000)
-          account.withdraw(1000)
-          expect(account.transactions[-1][:balance]).to eq(2000)
-        end
-      end
+    it 'logs debittransaction in the statement' do
+      allow(statement).to receive(:log_transaction)
+      account.deposit(1000)
+      expect(transaction_class).to receive(:create)
+      expect(statement).to receive(:log_transaction).with(transaction)
+      account.withdraw(1000)
     end
+
+    # describe 'adds a debit transaction to the list of transactions' do
+    #   it 'adds hash with debit key and 1000 value to transactions when 1000 is withdrawn' do
+    #     account.deposit(2000)
+    #     account.withdraw(1000)
+    #     expect(account.transactions[-1][:debit]).to eq(1000)
+    #   end
+
+    #   it 'adds hash with debit key and 2000 value to transactions when 2000 is withdrawn' do
+    #     account.deposit(3000)
+    #     account.withdraw(2000)
+    #     expect(account.transactions[-1][:debit]).to eq(2000)
+    #   end
+
+    #   describe 'transaction is dated' do
+    #     it 'adds hash with date key and a value of date formatted DD/MM/YYYY to transactions' do
+    #       account.deposit(2000)
+    #       account.withdraw(1000)
+    #       expect(account.transactions[-1][:date]).to match(%r{\d{2}/\d{2}/\d{4}})
+    #     end
+    #   end
+
+    #   describe 'transaction includes updated balance' do
+    #     it 'adds hash with balance key and updated 1000 balance as value to 2000 debit transaction from 3000 balance' do
+    #       account.deposit(3000)
+    #       account.withdraw(2000)
+    #       expect(account.transactions[-1][:balance]).to eq(1000)
+    #     end
+    #     it 'adds hash with balance key and updated 2000 balance as value to 1000 debit transaction from 3000 balance' do
+    #       account.deposit(3000)
+    #       account.withdraw(1000)
+    #       expect(account.transactions[-1][:balance]).to eq(2000)
+    #     end
+    #   end
+    # end
   end
 
-  describe '#print_statement' do
-    describe 'prints transactions' do
-      it "prints a statement header 'date || credit || debit || balance'" do
-        expect { account.print_statement }.to output(
-          "date || credit || debit || balance\n"
-        ).to_stdout
-      end
-      it "prints 'DD/MM/YYYY || 3000.00 || || 3000.00' as statement following single 3000 deposit" do
-        account.deposit(3000)
-        expect { account.print_statement }.to output(
-          %r{\d{2}/\d{2}/\d{4} \|\| 3000.00 \|\|  \|\| 3000.00}
-        ).to_stdout
-      end
+  # describe '#print_statement' do
+  #   describe 'prints transactions' do
+  #     it "prints a statement header 'date || credit || debit || balance'" do
+  #       expect { account.print_statement }.to output(
+  #         "date || credit || debit || balance\n"
+  #       ).to_stdout
+  #     end
+  #     it "prints 'DD/MM/YYYY || 3000.00 || || 3000.00' as statement following single 3000 deposit" do
+  #       account.deposit(3000)
+  #       expect { account.print_statement }.to output(
+  #         %r{\d{2}/\d{2}/\d{4} \|\| 3000.00 \|\|  \|\| 3000.00}
+  #       ).to_stdout
+  #     end
 
-      it "prints 'DD/MM/YYYY || 5000.00 || || 5000.00' as statement following single 3000 deposit" do
-        account.deposit(5000)
-        expect { account.print_statement }.to output(
-          %r{\d{2}/\d{2}/\d{4} \|\| 5000.00 \|\|  \|\| 5000.00}
-        ).to_stdout
-      end
+  #     it "prints 'DD/MM/YYYY || 5000.00 || || 5000.00' as statement following single 3000 deposit" do
+  #       account.deposit(5000)
+  #       expect { account.print_statement }.to output(
+  #         %r{\d{2}/\d{2}/\d{4} \|\| 5000.00 \|\|  \|\| 5000.00}
+  #       ).to_stdout
+  #     end
 
-      it 'prints multiple transactions each on separate lines most recent first' do
-        account.deposit(5000)
-        account.deposit(2000)
-        expect { account.print_statement }.to output(
-          %r{\d{2}/\d{2}/\d{4} \|\| 2000.00 \|\|  \|\| 7000.00\n\d{2}/\d{2}/\d{4} \|\| 5000.00 \|\|  \|\| 5000.00\n}
-        ).to_stdout
-      end
+  #     it 'prints multiple transactions each on separate lines most recent first' do
+  #       account.deposit(5000)
+  #       account.deposit(2000)
+  #       expect { account.print_statement }.to output(
+  #         %r{\d{2}/\d{2}/\d{4} \|\| 2000.00 \|\|  \|\| 7000.00\n\d{2}/\d{2}/\d{4} \|\| 5000.00 \|\|  \|\| 5000.00\n}
+  #       ).to_stdout
+  #     end
 
-      it 'prints both credit and debit transactions' do
-        account.deposit(5000)
-        account.withdraw(2000)
-        expect { account.print_statement }.to output(
-          %r{\d{2}/\d{2}/\d{4} \|\|  \|\| 2000.00 \|\| 3000.00\n\d{2}/\d{2}/\d{4} \|\| 5000.00 \|\|  \|\| 5000.00\n}
-        ).to_stdout
-      end
-    end
-  end
+  #     it 'prints both credit and debit transactions' do
+  #       account.deposit(5000)
+  #       account.withdraw(2000)
+  #       expect { account.print_statement }.to output(
+  #         %r{\d{2}/\d{2}/\d{4} \|\|  \|\| 2000.00 \|\| 3000.00\n\d{2}/\d{2}/\d{4} \|\| 5000.00 \|\|  \|\| 5000.00\n}
+  #       ).to_stdout
+  #     end
+  #   end
+  # end
+
+
 end
